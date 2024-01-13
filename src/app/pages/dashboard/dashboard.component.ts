f74c3621e80961ac479a2b725706ebaa89715a9f
LucasDMBorges <dev.lucasdean@gmail.com>
2024-01-13 13:33:19 -0300

    feat: :sparkles: Layout tela de login

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ContribuicaoMensalComponent } from './contribuicao-mensal/contribuicao-mensal.component';
 
 @Component({
   standalone: true,
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.less'],
+  imports: [ContribuicaoMensalComponent],
 })
 export class DashboardComponent {}
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   standalone: true,
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.less'],
+  imports: [CommonModule, InputTextModule],
 })
 export class LoginPageComponent {}
