6d1bb6717b239f958ff00be3d105ad4ccd1d0ca3
LucasDMBorges <dev.lucasdean@gmail.com>
2024-01-14 11:12:13 -0300

    feat: :sparkles: layout sidebar

---
+++ src/app/components/sidebar/sidebar-mock.const.ts
@@ -0,0 +1,24 @@
+export const PAGES = [
+  {
+    name: 'Ver Extrato',
+    icon: '../../../assets/svg/file-invoice-dollar.svg',
+  },
+  {
+    name: 'Contribuição Mensal',
+    icon: '../../../assets/svg/envelope-open-dollar.svg',
+  },
+  { name: 'Documentos', icon: '../../../assets/svg/file-alt.svg' },
+  { name: 'Regime de Tributação', icon: '../../../assets/svg/user-chart.svg' },
+  {
+    name: 'Solicitar Benefício',
+    icon: '../../../assets/svg/comment-dollar.svg',
+  },
+  {
+    name: 'Extrato Progressivo',
+    icon: '../../../assets/svg/file-chart-line.svg',
+  },
+  {
+    name: 'Informações',
+    icon: '../../../assets/svg/info.svg',
+  },
+];
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { DividerModule } from 'primeng/divider';
+import { ListboxModule } from 'primeng/listbox';
+import { PAGES } from './sidebar-mock.const';
 
 @Component({
   standalone: true,
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.less'],
+  imports: [ListboxModule, DividerModule],
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  public pages = PAGES;
+}
