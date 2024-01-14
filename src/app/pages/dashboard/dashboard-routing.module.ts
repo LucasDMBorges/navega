cf62fd3c346306d5556a4ebf4ff5c4579317849b
LucasDMBorges <dev.lucasdean@gmail.com>
2024-01-14 11:13:16 -0300

    feat: :sparkles: layout dashboard

--- src/app/pages/dashboard/dashboard-routing.module.ts
+++ src/app/pages/dashboard/dashboard-routing.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule, Routes } from '@angular/router';
 import { ContribuicaoMensalComponent } from './contribuicao-mensal/contribuicao-mensal.component';
+import { DashboardComponent } from './dashboard.component';
 
 export const DASHBOARD_ROUTES: Route[] = [
   {
     path: '',
     pathMatch: 'prefix',
+    component: DashboardComponent,
     children: [
-      {
-        path: '',
-        redirectTo: 'contribuicao-mensal',
-        pathMatch: 'full',
-      },
       { path: 'contribuicao-mensal', component: ContribuicaoMensalComponent },
     ],
   },
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ContribuicaoMensalComponent } from './contribuicao-mensal/contribuicao-mensal.component';
+import { HeaderComponent } from 'src/app/components/header/header.component';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { MenuItem } from 'primeng/api';
+import { SidebarComponent } from 'src/app/components/sidebar/sidebar.component';
 
 @Component({
   standalone: true,
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.less'],
-  imports: [ContribuicaoMensalComponent],
+  imports: [
+    HeaderComponent,
+    SidebarComponent,
+    ContribuicaoMensalComponent,
+    BreadcrumbModule,
+  ],
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  public items: MenuItem[] = [{ label: 'MEU PLANO' }];
+
+  public home: MenuItem = { icon: 'pi pi-arrow-left', routerLink: '/' };
+}
