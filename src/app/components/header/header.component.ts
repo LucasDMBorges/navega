ad0783be1433e12aaf42d1b896861a9fe76c303d
LucasDMBorges <dev.lucasdean@gmail.com>
2024-01-14 11:11:41 -0300

    feat: :sparkles: layout Header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   standalone: true,
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.less'],
+  imports: [AvatarModule],
 })
 export class HeaderComponent {}
